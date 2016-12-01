9c905a5eda60fae711149b8ebb3d781d487117e7
motohiroe shiobara <siobara.motohiro@gmail.com>
2016-12-01 22:48:03 +0900

    訂正

---
+++ lib/scraper.rb
@@ -0,0 +1,16 @@
+require 'line/bot'
+
+header = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer #{ENV["LINE_CHANNEL_ACCESS_TOKEN"]}"
+    }
+message = {
+  type: 'text',
+  text: 'hello'
+}
+client = Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_ACCESS_TOKEN"]
+}
+response = client.push_message("U5f76a050a6809546f1dcae891122b9bd", message)
+p response
