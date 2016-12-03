7f40897d6c7b54673c5c7b3837a40a5271558f5c
motohiroe shiobara <siobara.motohiro@gmail.com>
2016-12-03 18:53:40 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'sample#index'
-  post '/callback' => 'webhook#callback'
+  post '/line/callback' => 'webhook#callback'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
