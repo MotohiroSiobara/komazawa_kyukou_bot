04a20bb9bfcc95196671d141b8588e89eb4303ee
motohiroe shiobara <siobara.motohiro@gmail.com>
2016-12-13 19:18:24 +0900

    user

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -17,18 +17,19 @@ class WebhookController < ApplicationController
     text_message = params["events"][0]["message"]["text"]
     user_id = params["events"][0]["source"]["userId"]
     reply_token = params["events"][0]["replyToken"]
-    message = {
-      type: 'text',
-      text: text_message
-    }
-    logger.info(user_id)
-    logger.info(reply_token)
-    client = Line::Bot::Client.new { |config|
-        config.channel_secret = CHANNEL_SECRET
-        config.channel_token = ACCESS_TOKEN
-    }
-    response = client.reply_message(reply_token, message)
-    logger.info(response)
+    User.find_by_create(user_id: user_id)
+    # message = {
+    #   type: 'text',
+    #   text: text_message
+    # }
+    # logger.info(user_id)
+    # logger.info(reply_token)
+    # client = Line::Bot::Client.new { |config|
+    #     config.channel_secret = CHANNEL_SECRET
+    #     config.channel_token = ACCESS_TOKEN
+    # }
+    # response = client.reply_message(reply_token, message)
+    # logger.info(response)
   end
 
   private
